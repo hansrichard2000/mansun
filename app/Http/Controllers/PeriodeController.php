29fcf876f0ef0c444c74bf7bf8868718090add4d
Hans Richard Alim Natadjaja <hansrichard2000@gmail.com>
2020-12-07 20:46:57 +0700

    make minor change to admin folder in views

--- app/Http/Controllers/PeriodeController.php
+++ app/Http/Controllers/PeriodeController.php
@@ -14,7 +14,7 @@ class PeriodeController extends Controller
      */
     public function index()
     {
-        return view('admin.index');
+        return view('periode.index');
     }
 
     /**
