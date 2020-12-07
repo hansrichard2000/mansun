a3fe3c586add533071f7c05677c9c7c6a15a4ca5
Hans Richard Alim Natadjaja <hansrichard2000@gmail.com>
2020-12-07 20:19:03 +0700

    Input view for admin

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\PeriodeController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -14,5 +15,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return redirect()->route('periode.index');
 });
+
+Route::resource('periode', PeriodeController::class);
