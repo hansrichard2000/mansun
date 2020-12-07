7a54dbb38b9bcbe1699f2bfddddad646ce9d7bb3
Hans Richard Alim Natadjaja <hansrichard2000@gmail.com>
2020-12-07 20:42:49 +0700

    Input migration phase 2

--- database/migrations/2020_12_07_125923_create_tasks_table.php
+++ database/migrations/2020_12_07_125923_create_tasks_table.php
@@ -19,12 +19,6 @@ class CreateTasksTable extends Migration
             $table->text('deskripsi')->nullable();
             $table->date('deadline');
             $table->text('link_hasil_kerja')->nullable();
-//            $table->unsignedBigInteger('penanggung_jawab');
-//            $table->foreign('penanggung_jawab')->references('id')->on('users');
-//            $table->unsignedBigInteger('status_task_id');
-//            $table->foreign('status_task_id')->references('id')->on('status_tasks');
-//            $table->unsignedBigInteger('created_by');
-//            $table->foreign('created_by')->references('id')->on('users');
             $table->timestamps();
         });
     }
