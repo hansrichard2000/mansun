d9844a581847d739b58df1c459f6660ce439cfd9
Hans Richard Alim Natadjaja <hansrichard2000@gmail.com>
2020-12-12 14:48:18 +0700

    Fix login layout phase 2

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,48 +1,60 @@
-@extends('layouts.login')
-
-@section('judul')
-    Welcome at ManSUn
-@endsection
+@extends('layouts.app')
 
 @section('content')
-    <div class="row justify-content-center">
-        <div class="col-md-9 col-lg-12 col-xl-10">
-            <div class="card shadow-lg o-hidden border-0 my-5">
-                <div class="card-body p-0">
-                    <div class="row" style="height: 515px">
-                        <div class="col-lg-6 d-none d-lg-flex">
-                            <div class="flex-grow-1 bg-login-image" style="background: url(&quot;assets/img/Frame%206.jpg&quot;) center / contain;width: 800px;"></div>
-                        </div>
-                        <div class="col-lg-6">
-                            <div class="p-5">
-                                <div class="text-center">
-                                    <h4 class="text-dark mb-4">Welcome Back!</h4>
-                                </div>
-                                <form class="user" action="{{route('login')}}" method="POST">
-                                    <div class="form-group">
-{{--                                            <input id="exampleInputEmail" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>--}}
-                                            <input class="form-control form-control-user @error('email') is-invalid @enderror" type="email" id="exampleInputEmail" aria-describedby="emailHelp" placeholder="Enter Email Address..." name="email" required>
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-md-9 col-lg-12 col-xl-10">
+                <div class="card shadow-lg o-hidden border-0 my-5">
+                    <div class="card-body p-0">
+                        <div class="row" style="height: 515px">
+                            <div class="col-lg-6 d-none d-lg-flex">
+                                <div class="flex-grow-1 bg-login-image" style="background: url(&quot;assets/img/Frame%206.jpg&quot;) center / contain;width: 800px;"></div>
+                            </div>
+                            <div class="col-lg-6">
+                                <div class="p-5">
+                                    <div class="text-center">
+                                        <h4 class="text-dark mb-4">Welcome Back!</h4>
+                                    </div>
+                                    <form method="POST" action="{{ route('login') }}">
+                                        @csrf
+                                        <div class="form-group">
+                                            <input id="email" type="email" class="form-control w-100 @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" placeholder="Enter Email Address..." required autocomplete="email" autofocus>
                                             @error('email')
                                             <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
+                                                <strong>{{ $message }}</strong>
+                                            </span>
+                                            @enderror
+                                        </div>
+                                        <div class="form-group">
+                                            <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" placeholder="Password" required autocomplete="current-password">
+                                            @error('password')
+                                            <span class="invalid-feedback" role="alert">
+                                            <strong>{{ $message }}</strong>
                                         </span>
                                             @enderror
-                                    </div>
-                                    <div class="form-group">
-                                        <input class="form-control form-control-user" type="password" id="exampleInputPassword" placeholder="Password" name="password">
-                                    </div>
-                                    <div class="form-group">
-                                        <div class="custom-control custom-checkbox small">
+                                        </div>
+                                        <div class="form-group">
                                             <div class="form-check">
-                                                <input class="form-check-input custom-control-input" type="checkbox" id="formCheck-1">
-                                                <label class="form-check-label custom-control-label" for="formCheck-1">Remember Me</label>
+                                                <input class="form-check-input" type="checkbox" name="remember" id="remember" {{ old('remember') ? 'checked' : '' }}>
+
+                                                <label class="form-check-label" for="remember">
+                                                    {{ __('Remember Me') }}
+                                                </label>
                                             </div>
                                         </div>
-                                    </div><button class="btn btn-primary btn-block text-white btn-user" type="submit" style="background: #1789fc;">Login</button>
-                                    <hr>
-                                </form>
-                                <div class="text-center"></div>
-                                <div class="text-center"></div>
+                                        <div class="form-group row mb-0">
+                                            <button type="submit" class="btn bg-mansun-blue text-white w-100">
+                                                {{ __('Login') }}
+                                            </button>
+
+                                            @if (Route::has('password.request'))
+                                                <a class="btn btn-link" href="{{ route('password.request') }}">
+                                                    {{ __('Forgot Your Password?') }}
+                                                </a>
+                                            @endif
+                                        </div>
+                                    </form>
+                                </div>
                             </div>
                         </div>
                     </div>
