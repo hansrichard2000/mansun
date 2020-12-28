912861ecd2de3f81c547b4513714e43bd5392384
ReinerJasin <reinerjasin@gmail.com>
2020-12-28 12:07:10 +0800

    hanya menampilkan periode yang user terdaftar

--- app/Http/Controllers/User/PeriodeController.php
+++ app/Http/Controllers/User/PeriodeController.php
@@ -3,9 +3,11 @@
 namespace App\Http\Controllers\User;
 
 use App\Http\Controllers\Controller;
+use App\Models\DivisiRoleUser;
 use App\Models\Periode;
 use App\Models\Proker;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class PeriodeController extends Controller
 {
@@ -16,8 +18,15 @@ class PeriodeController extends Controller
      */
     public function index()
     {
-        $periodes = Periode::all();
-//        periode where user_divisi = proker divisi
+        //1. ngambil di divisi mana saja user terdaftar sebagai panitia
+        $dvs = DivisiRoleUser::all()->where('mansun_user_id',Auth::user()->id)->pluck('mansun_divisi_id')->toArray();
+
+        //2. ambil secara distinct semua id proker yg user itu terdaftar
+        $prokers = Proker::distinct()->whereIn('id', $dvs)->pluck('periode_id')->toArray();
+
+        //3. select distinct periode id berdasarkan array dari step 2
+        $periodes = Periode::all()->whereIn('id', $prokers);
+
         return view('periode.index', compact('periodes'));
     }
 
