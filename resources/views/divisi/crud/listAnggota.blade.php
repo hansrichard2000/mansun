6024f1683659b591e3da9fc9bad0aa4060a02fb7
Hans Richard Alim Natadjaja <hansrichard2000@gmail.com>
2020-12-28 11:58:30 +0700

    Fix tampilan lihat anggota

--- resources/views/divisi/crud/listAnggota.blade.php
+++ resources/views/divisi/crud/listAnggota.blade.php
@@ -11,7 +11,7 @@
 @section('content')
     <div class="container-fluid">
         <div class="d-sm-flex justify-content-between align-items-center mb-4">
-            <h3 class="text-dark mb-0 ml-3">Nama Divisi</h3>
+            <h3 class="text-dark mb-0 ml-3">{{$member->nama_divisi}}</h3>
             @auth
                 @if(\illuminate\Support\Facades\Auth::user()->isAdmin())
                     <button type="button" class="btn bg-mansun-blue text-white float-left mr-5" title="Add guest to this event"
