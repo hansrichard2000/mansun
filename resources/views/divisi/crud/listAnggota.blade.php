cbdf1e1e88c63fd47ac4da6039846ab4e14e97d1
ReinerJasin <reinerjasin@gmail.com>
2021-01-08 22:53:49 +0800

    harusnya udah betul actionnya kali ini

--- resources/views/divisi/crud/listAnggota.blade.php
+++ resources/views/divisi/crud/listAnggota.blade.php
@@ -55,13 +55,17 @@
                 <tbody>
                     @foreach($members as $member)
 {{--                        {{dd($member->user)}}--}}
-                        <tr class="text-center">
-                            <td>{{$member->user->id}}</td>
-                            <td>{{$member->user->student['name']}}</td>
-                            <td>{{$member->user->email}}</td>
-                            <td>{{$member->role->role}}</td>
                             @auth
                                 @if(\illuminate\Support\Facades\Auth::user()->isAdmin())
+                                    @foreach($members as $member)
+
+                                        <tr class="text-center">
+                                            <td>{{$member->user->id}}</td>
+                                            <td>{{$member->user->student['name']}}</td>
+                                            <td>{{$member->user->email}}</td>
+                                            <td>{{$member->role->role}}</td>
+                                        </tr>
+
                                     <td>
                                         <form action="{{route('admin.role.destroy', $member->id)}}" method="POST">
                                             {{ csrf_field() }}
@@ -69,9 +73,19 @@
                                             <button type="submit" class="btn btn-danger text-white" title="Delete this user" style="border-radius: 50%"><i class="fas fa-trash" aria-hidden="true" style="color: #ffffff"></i></button>
                                         </form>
                                     </td>
+                                    @endforeach
                                 @elseif(\illuminate\Support\Facades\Auth::user()->isUser())
 {{--                                    @foreach(\illuminate\Support\Facades\Auth::user()->roles as $userRoles)--}}
                                         @if($id == $member->divisi['id'] && $member->role['id']  == 1 && $member->user['id'] == \Illuminate\Support\Facades\Auth::user()->id)
+                                        @foreach($members as $member)
+
+                                            <tr class="text-center">
+                                                <td>{{$member->user->id}}</td>
+                                                <td>{{$member->user->student['name']}}</td>
+                                                <td>{{$member->user->email}}</td>
+                                                <td>{{$member->role->role}}</td>
+                                            </tr>
+
                                             <td>
                                                 <form action="{{route('user.role.destroy', $member->id)}}" method="POST">
                                                     {{ csrf_field() }}
@@ -79,11 +93,11 @@
                                                     <button type="submit" class="btn btn-danger text-white" title="Delete this user" style="border-radius: 50%"><i class="fas fa-trash" aria-hidden="true" style="color: #ffffff"></i></button>
                                                 </form>
                                             </td>
+                                        @endforeach
                                         @endif
 {{--                                    @endforeach--}}
                                 @endif
                             @endauth
-                        </tr>
                     @endforeach
                 </tbody>
             </table>
