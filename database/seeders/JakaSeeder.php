f99a05fb34dd8b45f35512ce81de2278fc9a6de9
BryanAntonnio <62532544+BryanAntonnio@users.noreply.github.com>
2021-01-12 19:27:40 +0700

    Jaka++

--- database/seeders/JakaSeeder.php
+++ database/seeders/JakaSeeder.php
@@ -14,9 +14,29 @@ class JakaSeeder extends Seeder
      */
     public function run()
     {
-        //Seeder Jaka
+        //Seeder Jaka Pengajar
         $jaka = new Jaka();
-        $jaka->name = 'Jaka1';
+        $jaka->name = 'Pengajar';
+        $jaka->save();
+
+        //Seeder Jaka Asisten Ahli
+        $jaka = new Jaka();
+        $jaka->name = 'Asisten Ahli';
+        $jaka->save();
+
+        //Seeder Jaka Lektor
+        $jaka = new Jaka();
+        $jaka->name = 'Lektor';
+        $jaka->save();
+
+        //Seeder Jaka Lektor Kepala
+        $jaka = new Jaka();
+        $jaka->name = 'Lektor Kepala';
+        $jaka->save();
+
+        //Seeder Jaka Professor
+        $jaka = new Jaka();
+        $jaka->name = 'Professor';
         $jaka->save();
     }
 }
